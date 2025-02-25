b71405fff12c6f4ebb5060554b39c551f3d887b7
Caroline-Gatwiri <caroline1gatwiri@gmail.com>
2025-02-25 11:08:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/practice/TemperatureConvertor.kt
@@ -0,0 +1,19 @@
+package com.example.practice
+
+fun main() {
+    printFinalTemperature(27.0, "Celsius", "Fahrenheit") { 9.0 / 5.0 * it + 32 }
+    printFinalTemperature(350.00,"Kelvin", "Celsius"){it - 273.15}
+    printFinalTemperature(10.00,"Fahrenheit", "Kelvin"){5.0/9.0 * (it - 32) + 273.15}
+}
+
+
+
+fun printFinalTemperature(
+    initialMeasurement: Double,
+    initialUnit: String,
+    finalUnit: String,
+    conversionFormula: (Double) -> Double
+) {
+    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
+    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
+}
